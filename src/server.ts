2d80980d0af3e19b64ca8c01a4040a124aa4070e
anptt <phamtruongthienan@gmail.com>
2022-06-30 11:26:16 +0700

    update util

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,27 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
 (async () => {
+
+  // Source : https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url
+  const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
 
   // Init the Express application
   const app = express();
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
+  //CORS Should be restricted
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:8100");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+  });
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
@@ -33,14 +42,38 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async (req, res) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  });
+
+  app.get("/filteredimage", async (req, res) => {
+    try {
+      const imageUrl = req.query.image_url;
+      console.log(`Image url ` + imageUrl);
+      if (!imageUrl || imageUrl == "") {
+        return res.status(400).send({ msg: `image_url is required` })
+      }
+
+      //exp : http://www.google.com
+      if (!imageUrl.match(urlRegex)) {
+        return res.status(402).send({ msg: `invalid image_url format` })
+      }
+
+      const savedLocalFile = await filterImageFromURL(imageUrl)
+
+      return res.status(200).sendFile(savedLocalFile, async () => {
+        console.log("delete file after response");
+        await deleteLocalFiles([savedLocalFile])
+      });
+    } catch (error) {
+      return res.status(500).send({ msg: `Err`, error })
+    }
+  });
+
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
