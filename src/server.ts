daf913546f9ff3694fc4badfaa2aca5e44631195
anptt <phamtruongthienan@gmail.com>
2022-08-06 01:31:11 +0700

    deploy EB and screenshots

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,8 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import bodyParser from 'body-parser';
 import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
 (async () => {
-
-  // Source : https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url
-  const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
 
   // Init the Express application
   const app = express();
@@ -16,12 +13,6 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  //CORS Should be restricted
-  app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8100");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-  });
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
@@ -37,37 +28,24 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+  app.get("/filteredimage/", async (req: Request, res: Response) => {
+    let { image_url }: any = req.query;
 
+    if (!image_url) {
+      return res.status(400)
+        .send(`Invalid url`);
+    }
+
+    const filteredPath = await filterImageFromURL(image_url);
+
+    res.sendFile(filteredPath, {}, () => deleteLocalFiles([filteredPath]));
+  });
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async (req, res) => {
+  app.get("/", async (req: Request, res: Response) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  });
-
-  app.get("/filteredimage", async (req, res) => {
-    try {
-      const imageUrl = req.query.image_url;
-      console.log(`Image url ` + imageUrl);
-      if (!imageUrl || imageUrl == "") {
-        return res.status(400).send({ msg: `image_url is required` })
-      }
-
-      //exp : http://www.google.com
-      if (!imageUrl.match(urlRegex)) {
-        return res.status(402).send({ msg: `invalid image_url format` })
-      }
-
-      const savedLocalFile = await filterImageFromURL(imageUrl)
-
-      return res.status(200).sendFile(savedLocalFile, async () => {
-        console.log("delete file after response");
-        await deleteLocalFiles([savedLocalFile])
-      });
-    } catch (error) {
-      return res.status(500).send({ msg: `Err`, error })
-    }
   });
 
 
